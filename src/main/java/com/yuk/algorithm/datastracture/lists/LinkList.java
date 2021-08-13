3aacacaa445fb9479d4b3752a663feccd67fe93b
yukyongsoo <yozx234@gmail.com>
2021-08-14 02:00:36 +0900

    add removeNthFromEnd

--- src/main/java/com/yuk/algorithm/datastracture/lists/LinkList.java
+++ src/main/java/com/yuk/algorithm/datastracture/lists/LinkList.java
@@ -11,4 +11,21 @@ public class LinkList {
 		}
 		return rev;
 	}
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		ListNode left = head;
+		ListNode right = head;
+		while(n -- > 0) {
+			right = right.next;
+		}
+
+		if(right == null) return head.next;
+
+		while(right.next != null) {
+			left = left.next;
+			right = right.next;
+		}
+		left.next = left.next.next;
+		return head;
+	}
 }
